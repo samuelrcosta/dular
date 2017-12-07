553fa5d542299573f4873a14d4768eaf6ea2c2bb
Samuel Rocha <samuel.rcosta@hotmail.com.br>
2017-12-07 21:54:37 -0200

    Correção de erros

--- controllers/revendedorController.php
+++ controllers/revendedorController.php
@@ -20,10 +20,10 @@ class revendedorController extends controller{
             $r->cadastrar($nome, $cpf, $rg, $celular, $telefone, $email, $endereco, $bairro, $cidade, $cep, $estado);
             $assunto = "Novo revendedor cadastrado no site";
             $mensagem = "Nome: ".$nome."<br>"."E-mail: ".$email;
-            $r->enviarEmail($this->MailName, $this->MailUsername, $assunto, $mensagem);
+            $r->enviarEmailComTemplate($this->MailName, $this->MailUsername, $assunto, $mensagem);
             $assunto = "Recebemos seu contato - Enxovais DuLar";
             $msg = "Olá ".$nome."<br>Recebemos seu contato para revenda.<br>Iremos analisar e responderemos o mais rápido possível.";
-            $r->enviarEmail($nome, $email, $assunto, $msg);
+            $r->enviarEmailComTemplate($nome, $email, $assunto, $msg);
             header("Location: ".BASE_URL."/revendedor/sucesso/".$nome);
         }
         $dados = array(
